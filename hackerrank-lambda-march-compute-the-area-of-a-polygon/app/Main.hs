a3b6955fda3978c3d86225f515ba07231c92a874
ernesto.alvarado <ernesto.alvarado@adroll.com>
2019-10-12 09:04:55 -0500

    aceptado

--- hackerrank-lambda-march-compute-the-area-of-a-polygon/app/Main.hs
+++ hackerrank-lambda-march-compute-the-area-of-a-polygon/app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Control.Monad
 
 -- XXX: https://www.wikihow.com/Calculate-the-Area-of-a-Polygon
+
 areaPoligono::[(Int,Int)]->Float
 -- XXX: https://stackoverflow.com/questions/3275193/whats-the-right-way-to-divide-two-int-values-to-obtain-a-float
 areaPoligono puntos = (fromIntegral ((suma1 x puntos True 0) - (suma1 y puntos False 0)))/2.0
